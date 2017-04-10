6c28bea81e40c0aae95ceb4f30d48cf583240d26
wei ching wang <andyw330@gmail.com>
2017-04-10 13:27:59 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # software_testing_hw2
+
+Unit testing for a plane ticket system.
---
+++ source/FoodOrder.java
@@ -0,0 +1,12 @@
+public class FoodOrder {
+
+	public final int price;
+	
+	public final String food;
+	
+	public FoodOrder(String tempFood, int tempPrice){
+		price=tempPrice;
+		food=tempFood;
+	}
+
+}
