f4cf9fa46f0a9b65e6fedf5a09300199c68efdfe
wei ching wang <andyw330@gmail.com>
2017-04-13 04:12:24 +0800

    add test for function showCode

--- test/RandomCodeUnitTest.java
+++ test/RandomCodeUnitTest.java
@@ -2,6 +2,9 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,6 +19,13 @@ public class RandomCodeUnitTest {
 	RandomCode r;
 	Seat[] seats;
 
+	private ByteArrayOutputStream outContent;
+
+	private void setUpStreams() {
+		outContent = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(outContent));
+	}
+
 	@Before
 	public void setUp() throws Exception {
 		r = new RandomCode(1);
@@ -27,6 +37,9 @@ public class RandomCodeUnitTest {
 
 	@After
 	public void tearDown() throws Exception {
+		r = null;
+		seats = null;
+		System.setOut(null);
 	}
 
 	// #seats <= 384
@@ -233,6 +246,158 @@ public class RandomCodeUnitTest {
 	public void getCodeTest_DecisionTableBasedTesting() {
 		r = new RandomCode(Integer.MAX_VALUE / 2);
 		assertEquals(Integer.MAX_VALUE / 2, r.getCode());
+	}
+
+
+	@Test
+	public void showCodeTest_BoundaryValueTesting() {
+		setUpStreams();
+		r = new RandomCode(1);
+		r.showCode();
+		assertEquals("00000001", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(2);
+		r.showCode();
+		assertEquals("00000002", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(Integer.MAX_VALUE / 2);
+		r.showCode();
+		assertEquals(Integer.toString(Integer.MAX_VALUE / 2), outContent.toString());
+		setUpStreams();
+		r = new RandomCode(Integer.MAX_VALUE - 1);
+		r.showCode();
+		assertEquals(Integer.toString((Integer.MAX_VALUE - 1)), outContent.toString());
+		setUpStreams();
+		r = new RandomCode(Integer.MAX_VALUE);
+		r.showCode();
+		assertEquals(Integer.toString(Integer.MAX_VALUE), outContent.toString());
+	}
+
+	// #digits = {1, 2, 3, 4, 5, 6, 7, 8, 9+}
+	@Test
+	public void showCodeTest_EquivalenceTesting() {
+		setUpStreams();
+		r = new RandomCode(1);
+		r.showCode();
+		assertEquals("00000001", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(12);
+		r.showCode();
+		assertEquals("00000012", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(123);
+		r.showCode();
+		assertEquals("00000123", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(1234);
+		r.showCode();
+		assertEquals("00001234", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(12345);
+		r.showCode();
+		assertEquals("00012345", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(123456);
+		r.showCode();
+		assertEquals("00123456", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(1234567);
+		r.showCode();
+		assertEquals("01234567", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(12345678);
+		r.showCode();
+		assertEquals("12345678", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(123456789);
+		r.showCode();
+		assertEquals("123456789", outContent.toString());
+	}
+
+	@Test
+	public void showCodeTest_EdgeValueTesting() {
+		setUpStreams();
+		r = new RandomCode(1);
+		r.showCode();
+		assertEquals("00000001", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(10);
+		r.showCode();
+		assertEquals("00000010", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(99);
+		r.showCode();
+		assertEquals("00000099", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(100);
+		r.showCode();
+		assertEquals("00000100", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(999);
+		r.showCode();
+		assertEquals("00000999", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(1000);
+		r.showCode();
+		assertEquals("00001000", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(9999);
+		r.showCode();
+		assertEquals("00009999", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(10000);
+		r.showCode();
+		assertEquals("00010000", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(99999);
+		r.showCode();
+		assertEquals("00099999", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(100000);
+		r.showCode();
+		assertEquals("00100000", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(999999);
+		r.showCode();
+		assertEquals("00999999", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(1000000);
+		r.showCode();
+		assertEquals("01000000", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(9999999);
+		r.showCode();
+		assertEquals("09999999", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(10000000);
+		r.showCode();
+		assertEquals("10000000", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(99999999);
+		r.showCode();
+		assertEquals("99999999", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(100000000);
+		r.showCode();
+		assertEquals("100000000", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(Integer.MAX_VALUE);
+		r.showCode();
+		assertEquals(Integer.toString(Integer.MAX_VALUE), outContent.toString());
+	}
+
+	// #digits < 8 => left padding to length 8 with zero
+	// #digits >= 8 => no left padding
+	@Test
+	public void showCodeTest_DecisionTableBasedTesting() {
+		setUpStreams();
+		r = new RandomCode(1234);
+		r.showCode();
+		assertEquals("00001234", outContent.toString());
+		setUpStreams();
+		r = new RandomCode(123456789);
+		r.showCode();
+		assertEquals("123456789", outContent.toString());
 	}
 
 }
